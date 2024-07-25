157dc03c856344c1fd949de748ab6d412531a717
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-07-25 21:08:40 +0530

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,6 +80,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
+          i am sandalu Thushan ekanayaka
           Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
