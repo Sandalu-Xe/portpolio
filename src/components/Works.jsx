6ced5f0d82dc8ff6feeec17fc590952a6c37b57e
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-08-02 23:48:09 +0530

    works chnges

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -85,7 +85,7 @@ const Works = () => {
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          and manage projects effectively.i am also have developed swift-ui app like whether app
         </motion.p>
       </div>
 
