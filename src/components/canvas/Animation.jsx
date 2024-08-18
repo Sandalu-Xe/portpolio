9f9a04d66deca0817bd4beb88fa6f77722c4f44b
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-08-18 13:16:16 -0700

    new anni

---
+++ src/components/canvas/Animation.jsx
@@ -0,0 +1,45 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Star = () => {
+  // Assuming you have a 3D model for a star or galaxy located in the public folder
+  const star = useGLTF("./earth_animation/scene.gltf");
+
+  return (
+    <primitive object={star.scene} scale={3} position-y={0} rotation-y={0} />
+  );
+};
+
+const AnimationCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 60,
+        near: 0.1,
+        far: 500,
+        position: [-5, 3, 7],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Star />
+
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default AnimationCanvas;
