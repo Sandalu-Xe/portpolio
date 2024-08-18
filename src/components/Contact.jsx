99a4ed04f47d473c1d88eab447f77b4d80ed05eb
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-08-18 12:42:03 -0700

    remove and adde

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -133,7 +133,8 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
-        <EarthCanvas />
+     <AstronomyCanvas/>
+     
 
         
       </motion.div>
--- src/components/canvas/Astronomy.jsx
+++ src/components/canvas/Astronomy.jsx
@@ -1,90 +1,44 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
-const Astronomy = () => {
-  const astronomy = useGLTF("./Asrtonomy_man/scene.gltf");
+const Star = () => {
+  // Assuming you have a 3D model for a star or galaxy located in the public folder
+  const star = useGLTF("./Asrtonomy_man/scene.gltf");
 
   return (
-    <primitive
-      object={astronomy.scene}
-      scale={2.5}
-      position-y={0}
-      rotation-y={0}
-    />
+    <primitive object={star.scene} scale={3} position-y={0} rotation-y={0} />
   );
 };
 
 const AstronomyCanvas = () => {
   return (
-    <>
-      <Canvas
-        shadows
-        frameloop="demand"
-        dpr={[1, 2]}
-        gl={{ preserveDrawingBuffer: true }}
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 200,
-          position: [-4, 3, 6],
-        }}
-      >
-        <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls
-            autoRotate
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Astronomy />
-          <Preload all />
-        </Suspense>
-      </Canvas>
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 60,
+        near: 0.1,
+        far: 500,
+        position: [-5, 3, 7],
+      }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Star />
 
-      {/* Sketchfab embed */}
-      <div className="sketchfab-embed-wrapper" style={{ marginTop: "20px" }}>
-        <iframe
-          title="Tripo Astronaut (animated)"
-          frameBorder="0"
-          allowFullScreen
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-          allow="autoplay; fullscreen; xr-spatial-tracking"
-          src="https://sketchfab.com/models/bf065335fc194bfeac12439b804f6135/embed?ui_theme=dark"
-          style={{ width: "640px", height: "480px" }}
-        ></iframe>
-        <p style={{ fontSize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A" }}>
-          <a
-            href="https://sketchfab.com/3d-models/tripo-astronaut-animated-bf065335fc194bfeac12439b804f6135?utm_medium=embed&utm_campaign=share-popup&utm_content=bf065335fc194bfeac12439b804f6135"
-            target="_blank"
-            rel="nofollow"
-            style={{ fontWeight: "bold", color: "#1CAAD9" }}
-          >
-            Tripo Astronaut (animated)
-          </a>{" "}
-          by{" "}
-          <a
-            href="https://sketchfab.com/jungle_jim?utm_medium=embed&utm_campaign=share-popup&utm_content=bf065335fc194bfeac12439b804f6135"
-            target="_blank"
-            rel="nofollow"
-            style={{ fontWeight: "bold", color: "#1CAAD9" }}
-          >
-            Jungle Jim
-          </a>{" "}
-          on{" "}
-          <a
-            href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=bf065335fc194bfeac12439b804f6135"
-            target="_blank"
-            rel="nofollow"
-            style={{ fontWeight: "bold", color: "#1CAAD9" }}
-          >
-            Sketchfab
-          </a>
-        </p>
-      </div>
-    </>
+        <Preload all />
+      </Suspense>
+    </Canvas>
   );
 };
 
