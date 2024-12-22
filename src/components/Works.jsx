8a1d9e30d19ee7f88e91a26735bc8fc3e2ab5321
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2024-12-23 03:55:35 +0530

    done the group project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -95,6 +95,31 @@ const Works = () => {
         ))}
       </div>
 
+      <motion.div variants={textVariant()}>
+
+        <h2 className={`${styles.sectionHeadText}`}>Group Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          i am sandalu Thushan ekanayaka
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.i am also have developed swift-ui app like whether app
+        </motion.p>
+      </div>
+
+      <div className='mt-10 flex flex-wrap '>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+
       
     </>
   );
