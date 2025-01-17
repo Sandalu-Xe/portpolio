c765a49e9891d9f1a67942514bf5f2a48c686cba
Thushan Ekanayaka <sandaluthushan20@gmail.com>
2025-01-17 16:36:33 +0530

    refersh code

--- src/components/TeamWork.jsx
+++ src/components/TeamWork.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Tilt } from 'react-tilt';
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github,weblink } from "../assets";
+import { github, weblink } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { teamworks } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
@@ -16,9 +16,6 @@ const ProjectCard = ({
   source_code_link,
   webpage,
   pointss,
-  
-
-
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
@@ -28,54 +25,54 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-        <div className='relative w-full h-[230px]'>
+        <div className="relative w-full h-[230px]">
           <img
             src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
+            alt={`${name} project`}
+            className="w-full h-full object-cover rounded-2xl"
           />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={github}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="Source Code"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
             <div
               onClick={() => window.open(webpage, "_blank")}
-              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={weblink}
-                alt='source code'
-                className='w-1/2 h-1/2 object-contain'
+                alt="Live Webpage"
+                className="w-1/2 h-1/2 object-contain"
               />
             </div>
           </div>
         </div>
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-              
-        <ul className="mt-5 list-disc ml-5 space-y-2">
-          {pointss.map((point, index) => (
-            <li
-              key={`project-point-${index}`}
-              className="text-white-100 text-[14px] pl-1 tracking-wider"
-            >
-              {point}
-            </li>
-          ))}
-        </ul>
-        </div>  
 
-        <div className='mt-4 flex flex-wrap gap-2'>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <ul className="mt-5 list-disc ml-5 space-y-2">
+            {pointss.map((point, index) => (
+              <li
+                key={`project-point-${index}`}
+                className="text-white-100 text-[14px] pl-1 tracking-wider"
+              >
+                {point}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
@@ -85,10 +82,6 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-    
-     
-        
-    
       </Tilt>
     </motion.div>
   );
@@ -98,22 +91,21 @@ const TeamWork = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-  
-        <h2 className={`${styles.sectionHeadText}`}>Group Projects.</h2>
+        <h2 className={`${styles.sectionHeadText}`}>Group Projects</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
-          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-         I have done  some project with my university friends. 
-         I have got more expirience about team work and we have used project management tools like gira 
-         ,click up 
+          I have collaborated on several projects with my university friends. 
+          These projects provided me with valuable experience in teamwork and project management. 
+          We utilized tools like Jira and ClickUp to effectively manage our tasks and workflows.
         </motion.p>
       </div>
 
-      <div className='mt-10 flex flex-wrap '>
+      <div className="mt-10 flex flex-wrap">
         {teamworks.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
